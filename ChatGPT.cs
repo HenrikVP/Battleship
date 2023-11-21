95a090e09e4dc23b41f3cf9c87673a287d1658c8
HenrikVP <hpo@tec.dk>
2023-11-21 08:17:14 +0100

    Add project files.

---
+++ ChatGPT.cs
@@ -0,0 +1,122 @@
+﻿namespace Battleship
+{
+    class ChatGPT
+    {
+        static int boardSize = 10;
+        static char[,] playerBoard = new char[boardSize, boardSize];
+        static char[,] enemyBoard = new char[boardSize, boardSize];
+        static int numberOfShips = 5;
+
+        public static void Game()
+        {
+            InitializeBoards();
+            PlaceShips();
+
+            Console.WriteLine("Welcome to Battleship Game!");
+
+            do
+            {
+                Console.WriteLine("\nPlayer Board:");
+                DisplayBoard(playerBoard);
+
+                Console.WriteLine("\nEnemy Board:");
+                DisplayBoard(enemyBoard);
+
+                Console.WriteLine("\nEnter coordinates to fire (e.g., A3):");
+                string input = Console.ReadLine().ToUpper();
+
+                if (input.Length == 2 && char.IsLetter(input[0]) && char.IsDigit(input[1]))
+                {
+                    int row = input[0] - 'A';
+                    int col = input[1] - '0';
+
+                    if (row >= 0 && row < boardSize && col >= 0 && col < boardSize)
+                    {
+                        if (enemyBoard[row, col] == 'S')
+                        {
+                            Console.WriteLine("Hit!");
+                            enemyBoard[row, col] = 'X';
+                            numberOfShips--;
+
+                            if (numberOfShips == 0)
+                            {
+                                Console.WriteLine("Congratulations! You sunk all enemy ships.");
+                                break;
+                            }
+                        }
+                        else if (enemyBoard[row, col] == 'X')
+                        {
+                            Console.WriteLine("You already fired at this location. Try again.");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Miss!");
+                            enemyBoard[row, col] = 'O';
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Invalid coordinates. Try again.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid input. Please enter coordinates in the format A3.");
+                }
+
+            } while (numberOfShips > 0);
+
+            Console.WriteLine("Game Over. Thanks for playing!");
+        }
+
+        static void InitializeBoards()
+        {
+            for (int i = 0; i < boardSize; i++)
+            {
+                for (int j = 0; j < boardSize; j++)
+                {
+                    playerBoard[i, j] = '-';
+                    enemyBoard[i, j] = '-';
+                }
+            }
+        }
+
+        static void DisplayBoard(char[,] board)
+        {
+            Console.Write("  ");
+            for (int i = 0; i < boardSize; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.WriteLine();
+
+            for (int i = 0; i < boardSize; i++)
+            {
+                Console.Write((char)('A' + i) + " ");
+                for (int j = 0; j < boardSize; j++)
+                {
+                    Console.Write(board[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+
+        static void PlaceShips()
+        {
+            Random random = new Random();
+
+            for (int i = 0; i < numberOfShips; i++)
+            {
+                int row, col;
+
+                do
+                {
+                    row = random.Next(0, boardSize);
+                    col = random.Next(0, boardSize);
+                } while (enemyBoard[row, col] == 'S');
+
+                enemyBoard[row, col] = 'S';
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿namespace Battleship
+{
+    internal class Program
+    {
+        enum Piece { Pawn, Rook, Knight, Bishop, Queen, King }
+        enum ChessColor { Black, White }
+
+        static void Main(string[] args)
+        {
+            ChatGPT.Game();
+
+            Console.WriteLine("Hello, World!");
+
+            // Skak 
+            //2 arrays af 16 arrays af 3 værdier
+            //                     {c, {p, {x, y, T}}}
+            int[,,] array2 = new int[2, 16, 3];
+
+            //32 arrays af 4 værdier
+            //                      p {x,y,T,c}
+            int[,] array = new int[32, 4];
+
+            Console.WriteLine(
+                $"Piece {(Piece)array[0, 2]} " +
+                $"Color {(ChessColor)array[0, 3]} " +
+                $"Pos (x,y) {array[0, 0]}, {array[0, 1]}");
+
+
+            int[,] numbers = { { 1, 4, 2 }, { 3, 6, 8 } };
+
+            for (int j = 0; j < numbers.GetLength(1); j++)
+            {
+                Console.WriteLine(numbers[1, j]);
+            }
+
+            //   2 players with each 17 spots with 2 coordinates P,#,{x,y}
+                        //Player, Shippart, X,Y
+            int[,,] ships = new int[2, 3, 2];
+            int[,,] ships3 = new int[2, 3, 2];
+
+            int[,,] shipsP1 = { { { 1, 2 }, { 4, 5 }, { 7, 3 } }, { { 1, 2 }, { 4, 5 }, { 7, 3 } } };
+
+            //int[,,] ships = { { { { 3,5 } },{ },{ } }, { } };
+            ships[0, 0, 0] = 3;
+            ships[0, 0, 1] = 5;
+
+            ships[0, 1, 0] = 3;
+            ships[0, 1, 1] = 6;
+
+
+            int[,] p1Ships = new int[17, 2];
+            int[,] p2Ships = new int[17, 2];
+            int[,] p1Shots = new int[100, 2];
+            int[,] p2Shots = new int[100, 2];
+
+            int[,,] ships2 = new int[2, 17, 2];
+
+            ships2[0, 0, 0] = 4;
+            ships2[0, 0, 1] = 8;
+
+            //ships2[]
+
+            //                     P, { x,y,T}
+            int[,] hits = new int[2, 3];
+                                //player, hit/ship, {x,y}
+            int[,,] all = new int[2, 2, 2];
+
+            //Board 10x10
+            //5 skibe, 17 felter (patrolboat2, submarine3, destroyer3, battleship4, carrier5)
+            //Player 1 sets a ship at 3,5
+
+            //Kryds og bolle
+            //int[,] kogb = new[];
+        }
+        enum Battleship { Empty, Hit, P}
+    }
+}
